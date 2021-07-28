0fb79d8b01717ff5846d75111c2dcb28ae09b7dd
ashleigh <ashleighdevito@gmail.com>
2021-07-27 22:40:24 -0400

    functional

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ app = Flask(__name__)
 
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
+
 @app.route("/")
 def home():
 
-    mars_data = mongo.db.collection.find_one()
+    mission_data = mongo.db.collection.find_one()
 
-    return render_template("index.html", mars = mars_data)
+    return render_template("index.html", mars = mission_data)
 
 
 @app.route("/scrape")
 def scrape():
-    mars_dict = scrape_mars.scrape()
+    mars_data = scrape_mars.scrape()
 
-    mongo.db.collection.update({}, mars_dict, upsert=True)
+    mongo.db.collection.update({}, mars_data, upsert=True)
 
     return redirect("/")
-
 
 
 if __name__ == "__main__":
